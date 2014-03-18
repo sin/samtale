c038b56c9888ed55b09ef5958f539d95a3fc7386
sin <mateusz-sienkan@o2.pl>
2014-03-18 07:28:24 +0100

    Network optimization

--- app/app.js
+++ app/app.js
@@ -13,16 +13,26 @@ app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+app.use(express.favicon(__dirname + '/../static/favicon.ico', { maxAge: 345600000 }));
 app.use(logger('dev'));
 app.use(express.compress());
+
+app.use(function (req, res, next) {
+    if (req.url.indexOf('/js/') === 0 || req.url.indexOf('/css/') === 0) {
+        res.setHeader("Cache-Control", "public, max-age=345600");
+        res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+    }
+    next();
+});
+
 app.use(express.static(__dirname + '/../static'));
 
 app.get('/', function (req, res) {
-    res.render('index', { title: "Samtale" });
+    res.render('index');
 
 });
 
-app.use(function(req, res){
+app.use(function (req, res) {
     res.send(404);
 });
 
